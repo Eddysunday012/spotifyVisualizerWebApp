3fdfca20648cf4c4f59f6cab90b768c1e0bdf4c5
Ethan Domingo <etddomin@ucsc.edu>
2023-08-30 12:43:51 -0700

    seems like things got updated

--- packages/spotify-functions/src/spotify.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect, vi} from 'vitest';
-import { getAccessToken } from './spotify';
-
-describe('spotify test suite', () => {
-    it("should return the token", async () => {
-        const token = await getAccessToken()
-        expect(token).toBe("hello world");
-    })
-})
--- packages/spotify-functions/src/spotify.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export async function getAccessToken() : Promise<string> {
-    return "hello world";
-  }
