981724754294de6f44c8c532c595ea99c4f570ae
Ethan Domingo <etddomin@ucsc.edu>
2023-10-06 16:58:31 -0700

    Still figuring out d3js

--- apps/genreBreakdown-dev/src/App.tsx
+++ apps/genreBreakdown-dev/src/App.tsx
@@ -4,10 +4,17 @@ import React from "react";
 
 function App(): JSX.Element {
   const GenreBreakdownInfo = { bruh: "bruh" };
+  const data = [
+    { axis: "Axis 1", value: 5 },
+    { axis: "Axis 2", value: 8 },
+    { axis: "Axis 3", value: 9 },
+    { axis: "Axis 4", value: 9 },
+    { axis: "Axis 5", value: 5 },
+  ];
   return (
     <>
       <DependenciesContext.Provider value={GenreBreakdownInfo}>
-        <GenreBreakdown />
+        <GenreBreakdown data={data} width={400} height={400} />
       </DependenciesContext.Provider>
     </>
   );
